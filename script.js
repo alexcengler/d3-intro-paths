16806d07bdcdccc50e175eeee46702789b0dd6e3
alexcengler <alexcengler@gmail.com>
2017-11-08 12:57:10 -0600

    Data loading with d3.queue and .defer

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ var width = 625 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
 
 
+d3.queue()
+  .defer(d3.json, 'data/fam-w-children-tanf-ratio.json')
+  .defer(d3.json, 'data/state_tanf_to_poverty_ratio.json')
+  .defer(d3.json, 'data/us-states.json')
+  .awaitAll(function (error, results) {
+    if (error) { throw error; }
+    
+    scatter = new DirectedScatterPlot(results[0]);
+
+  });
+
+
+function DirectedScatterPlot(data) {
+
+  console.log(data);
+
+};
+
